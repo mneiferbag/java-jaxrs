d9c7f30cd5bd070ef0efdbbfc7292cce74cee90f
Markus Neifer <mneiferbag@users.noreply.github.com>
2021-04-03 11:29:37 +0000

    Add generated files

---
+++ src/main/java/de/mneifercons/examples/jaxrs/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021 Markus Neifer
+ * Licensed under the MIT License.
+ * See file LICENSE in parent directory of project root.
+ */
+
+/**
+ * Package with Java JAX-RS code examples.
+ */
+package de.mneifercons.examples.jaxrs;
